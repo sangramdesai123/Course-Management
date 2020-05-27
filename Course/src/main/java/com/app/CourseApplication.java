350253d4cd749d95c5dea6436851f1550401cc5e
sangramdesai123 <sangram.desai@somaiya.edu>
2020-05-28 00:22:00 +0530

    java login backend ready

--- Course/src/main/java/com/app/CourseApplication.java
+++ Course/src/main/java/com/app/CourseApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseApplication {
-
+	/*
+	 * My login and register user urls
+	 * 
+	 * http://localhost:8080/registeruser
+	 * 
+	 * http://localhost:8080/login
+	 * 
+	 * */
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
