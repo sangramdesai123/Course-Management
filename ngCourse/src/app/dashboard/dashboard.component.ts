34b164ee481893fa658be41b2845652abbe1feae
sangramdesai123 <sangram.desai@somaiya.edu>
2020-05-30 01:54:41 +0530

    search course by id code added,there is bug

--- ngCourse/src/app/dashboard/dashboard.component.ts
+++ ngCourse/src/app/dashboard/dashboard.component.ts
@@ -9,13 +9,19 @@ import { RegistrationService } from '../registration.service';
 export class DashboardComponent implements OnInit {
 
   courselist:any;
+  id:any;
   constructor(private _service: RegistrationService) { }
 
   public deleteCourse(id:number){
     let resp=this._service.deleteCourse(id);
     resp.subscribe((data)=> this.courselist=data);
   }
-  
+
+  public searchCoursebyId(){
+    let resp=this._service.getCourseById(this.id);
+    resp.subscribe((data)=> this.courselist=data);
+  }
+
   ngOnInit(): void {
     let resp=this._service.getAllCourse();
     resp.subscribe((data)=> this.courselist=data);
