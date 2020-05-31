526c12af71851d0d289d2c0c0379f9afafb58437
sangramdesai123 <sangram.desai@somaiya.edu>
2020-05-31 20:25:41 +0530

    search matrial by Course name feature completed

--- ngCourse/src/app/tranningmatrial/tranningmatrial.component.ts
+++ ngCourse/src/app/tranningmatrial/tranningmatrial.component.ts
@@ -8,10 +8,16 @@ import { TrainingService } from '../training.service';
 })
 export class TranningmatrialComponent implements OnInit {
   matriallist:any;
+  course:String;
   constructor(private _service:TrainingService) { }
 
   public deleteMatrial(course:string){
     let resp=this._service.deleteMatrial(course);
+    resp.subscribe((data)=> this.matriallist=data);
+  }
+
+  public searchMatrialbyCourse(){
+    let resp=this._service.getCourseByCourse(this.course);
     resp.subscribe((data)=> this.matriallist=data);
   }
 
